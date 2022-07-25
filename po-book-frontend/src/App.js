e27be5ba25afb44c4cfeccb34cbfa171ed2d9cb8
Mursalin Shaikh <mursalinshaikh5286@hotmail.com>
2022-07-25 19:23:44 -0400

    home page initial design update

---
+++ po-book-frontend/src/App.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/App.css';
+import './css/runwebrun.css';
+
+// 
+
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Home />
+      <Footer/>
+
+    </>
+
+  );
+}
+
+export default App;
